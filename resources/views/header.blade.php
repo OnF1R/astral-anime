e281416ff50ca85f7fb2b1e13473d5894bd47461
OnF1R <bender987@bk.ru>
2022-07-26 17:32:57 +0300

    Header file

---
+++ resources/views/header.blade.php
@@ -0,0 +1,12 @@
+<nav class="navbar mt-3 sticky-top">
+        <div class="container header">
+            <a href="{{ route('anime.main') }}" class="d-flex align-items-center mb-2 mb-lg-0 text-decoration-none">
+                <h3>Astral</h3>
+                <img src="{{asset('images/logo/logo.png')}}" width="96" height="96" class="rounded-circle ms-4" alt="">
+            </a>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
