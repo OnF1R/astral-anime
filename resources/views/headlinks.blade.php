d1a6d5c0bc7df792f7653dc8045bf28ec02edaef
OnF1R <bender987@bk.ru>
2022-07-26 17:33:11 +0300

    CSS & JS Links file

---
+++ resources/views/headlinks.blade.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/script.js') }}"></script>
