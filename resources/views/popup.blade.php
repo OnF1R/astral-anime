846b4d30564541bde408e4da03a222e58d3a592c
OnF1R <bender987@bk.ru>
2022-07-31 20:34:41 +0300

    Update animeData to anime

--- resources/views/popup.blade.php
+++ resources/views/popup.blade.php
@@ -1,16 +1,16 @@
 <!-- Modal -->
 @php
-$jsonAnimeDetails = file_get_contents($getAnimeDetails . $animeData[$i]['animeId']);
+$jsonAnimeDetails = file_get_contents($getAnimeDetails . $anime['animeId']);
 $animeDetails = json_decode($jsonAnimeDetails,true);
 @endphp
-<div class="modal fade" id="id-{{ $animeData[$i]['animeId'] }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="id-{{ $anime['animeId'] }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-5 position-relative"><img src="{{ $animeData[$i]['animeImg'] }}" alt="" class="img_main_anime_display_hot" style="height: 400px;">
-                    <div class="position-absolute popup_buttons"><a href="{{ route('anime.page', ['id' => $animeData[$i]['animeId']])}}"><button type="button" class="btn btn-primary">Watch</button></a>
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div class="col-5 position-relative"><img src="{{ $anime['animeImg'] }}" alt="" class="img_main_anime_display_hot" style="height: 400px;">
+                    <div class="position-absolute popup_buttons"><a href="{{ route('anime.page', ['id' => $anime['animeId']])}}"><button type="button" class="btn popup_watch_button">Watch</button></a>
+                    <button class="btn popup_watch_button dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                                 Add to list
                             </button>
                             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
@@ -20,9 +20,7 @@ $animeDetails = json_decode($jsonAnimeDetails,true);
                                 <li><a class="dropdown-item" href="#">Abandoned</a></li>
                             </ul>
                         </div>
-                    </div>
-                        
-                            
+                    </div>   
                     <div class="col popup_anime_info pt-3">
                         <p><strong>Title: {{ $animeDetails['animeTitle'] }}</strong></p>
                         <p>@if(!empty($animeDetails['otherNames'])) Other names: {{ $animeDetails['otherNames'] }}@endif</p>
@@ -33,7 +31,6 @@ $animeDetails = json_decode($jsonAnimeDetails,true);
                         <p>Episodes availible: {{ $animeDetails['totalEpisodes'] }}</p>
                         <p></p>
                     </div>
-                    
                 </div>
                 <div class="w-100 mt-3"></div>
                     <div class="col popup_anime_synopsis p-3">
