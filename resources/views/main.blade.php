9e727cc475d77eee1690ecc271d29373f0c5bc0e
Oleg <bender987@bk.ru>
2022-08-28 13:55:56 +0300

    Update main works with new popups

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,80 +8,87 @@
     @include ('headlinks')
     <title>Document</title>
 </head>
+
 <body>
     @include('header')
     <div class="main_anime_trend_container container-fluid mt-5 text-center fs-2 text-color-blue">TRENDING</div>
     <div class="main_anime_trend_container container-fluid mt-4  position-relative">
 
-        
+        @foreach ($topAiringData as $anime)
+        <!-- Popup -->
+        @endforeach
         <div class="container">
             <div class="row anime_block rounded text-center">
                 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner" role="listbox">
-                    @foreach ($topAiringData as $anime)
+                        @foreach ($topAiringData as $anime)
                         @if ($loop->first) <div class="carousel-item active"> @else <div class="carousel-item"> @endif
-                        <div class="col rounded_main_popular position-relative popular_anime_main scale m-2 p-0">
-                            <div class="works_item">
-                                <img loading="lazy" src="{{ $anime['animeImg'] }}"class="img_main_anime_display_hot works_image" alt="">
-                                <div class="works_info">
-                                    <div class="works_title">{{ $anime['animeTitle'] }}</div>
-                                    <button type="button" class="btn main_page_watch_button" data-bs-toggle="modal" data-bs-target="#id-{{$anime['animeId']}}">
-                                        More...
-                                    </button>
+                                <div class="col rounded_main_popular position-relative popular_anime_main scale m-2 p-0">
+                                    <div class="works_item">
+                                        <img loading="lazy" src="{{ $anime['animeImg'] }}" class="img_main_anime_display_hot works_image" alt="">
+                                        <div class="works_info">
+                                            <div class="works_title">{{ $anime['animeTitle'] }}</div>
+                                            <button name="{{ $anime['animeId'] }}" onclick="loadPopup(this)" type="button" class="btn main_page_watch_button" data-bs-toggle="modal" data-bs-target="#popupAnime">
+                                                More...
+                                            </button>
+                                            <a href="{{ route('anime.page', ['id' => $anime['animeId']])}}"><button type="button" class="btn popup_watch_button mt-1">Watch</button></a>
+                                        </div>
+                                    </div>
+                                    <!-- <object data="{{asset('images/svg/fire.svg')}}" width="64" height="64" class="hot-svg"> </object> -->
+                                    <div class="w-100"></div>
+                                    <div class="col rounded mt-2">
+                                        <p class="p-1 anime_name">{{ $anime['animeTitle'] }}</p>
+                                    </div>
                                 </div>
                             </div>
-                                <!-- <object data="{{asset('images/svg/fire.svg')}}" width="64" height="64" class="hot-svg"> </object> -->
-                                <div class="w-100"></div>
-                            <div class="col rounded mt-2"><p class="p-1 anime_name">{{ $anime['animeTitle'] }}</p></div>
+                            @endforeach
                         </div>
-                        </div>
-                        @endforeach
-                        @php
-                            $jsonAnimeDetails = file_get_contents($getAnimeDetails . $anime['animeId']);
-                            $animeDetails = json_decode($jsonAnimeDetails,true);
-                            @include('popup')
-                        @endphp
+                    </div>
                 </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-        </div>
-            
-    </div>
-</div>
 
-<div class="main_anime_trend_container container-fluid mt-5 text-center fs-2 text-color-blue">ANIME LIST</div>
-<div class="main_anime_list_container container-fluid mt-4 position-relative">
-    <div class="container">
-                @foreach($animeData as $anime)
-                @if($loop->first || $loop->index % 5 == 0) <div class="row text-center"> @endif
+        </div>
+    </div>
+
+    <div class="main_anime_trend_container container-fluid mt-5 text-center fs-2 text-color-blue">ANIME LIST</div>
+    <div class="main_anime_list_container container-fluid mt-4 position-relative">
+        <div class="container">
+            @foreach($animeData as $anime)
+
+            @if($loop->first || $loop->index % 5 == 0) <div class="row text-center"> @endif
                 <div class="col rounded_main_popular position-relative popular_anime_main scale m-2 p-0">
-                            <div class="works_item">
-                                <img loading="lazy" src="{{ $anime['animeImg'] }}" class="img_main_anime_display_hot works_image" alt="">
-                                <div class="works_info">
-                                    <div class="works_title">{{ $anime['animeTitle'] }}</div>
-                                    <button type="button" class="btn main_page_watch_button" data-bs-toggle="modal" data-bs-target="#id-{{$anime['animeId']}}">
-                                        More...
-                                    </button>
-                                </div>
-                            </div>
-                                <!-- <object data="{{asset('images/svg/fire.svg')}}" width="64" height="64" class="hot-svg"> </object> -->
-                                <div class="w-100"></div>
-                            <div class="col rounded mt-2"><p class="p-1 anime_name">{{ $anime['animeTitle'] }}</p></div>
+                    <div class="works_item">
+                        <img loading="lazy" src="{{ $anime['animeImg'] }}" class="img_main_anime_display_hot works_image" alt="">
+                        <div class="works_info">
+                            <div class="works_title">{{ $anime['animeTitle'] }}</div>
+                            <button name="{{ $anime['animeId'] }}" onclick="loadPopup(this);" type="button" class="btn main_page_watch_button" data-bs-toggle="modal" data-bs-target="#popupAnime">
+                                More...
+                            </button>
+                            <a href="{{ route('anime.page', ['id' => $anime['animeId']])}}"><button type="button" class="btn popup_watch_button mt-1">Watch</button></a>
                         </div>
-                @include('popup')
-                @if($loop->iteration % 5 == 0) </div> @endif
-                @endforeach
+                    </div>
+                    <!-- <object data="{{asset('images/svg/fire.svg')}}" width="64" height="64" class="hot-svg"> </object> -->
+                    <div class="w-100"></div>
+                    <div class="col rounded mt-2">
+                        <p class="p-1 anime_name">{{ $anime['animeTitle'] }}</p>
+                    </div>
+                </div>
+                <!-- Popup -->
+                @if($loop->iteration % 5 == 0)
+            </div> @endif
+            @endforeach
         </div>
     </div>
     @include ('footer')
 </body>
+@include('fetch-api-popup')
 <script src="{{ asset('js/carousel-script.js') }}"></script>
 </html>
